8c24286c98396fca7a6ba2e65ba12759f3419e3f
Ajay Prakash <80052150+Ajayprakashk7@users.noreply.github.com>
2023-10-15 21:32:16 +0530

    fix

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/ajayprakash/Documents/GitHub/Ecommerce-Store/app/layout.tsx": [
+      "/Users/ajayprakash/Documents/GitHub/Ecommerce-Store/app/globals.css",
+      "/Users/ajayprakash/Documents/GitHub/Ecommerce-Store/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Urbanist\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"font\"}"
+    ]
+  },
+  "cssModules": {
+    "/Users/ajayprakash/Documents/GitHub/Ecommerce-Store/app/(routes)/page": [
+      "/Users/ajayprakash/Documents/GitHub/Ecommerce-Store/app/globals.css",
+      "/Users/ajayprakash/Documents/GitHub/Ecommerce-Store/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Urbanist\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"font\"}"
+    ]
+  }
+}
---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,10 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {},
+  "app": {
+    "/Users/ajayprakash/Documents/GitHub/Ecommerce-Store/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Urbanist\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"font\"}": [
+      "static/media/01af0fc7b4278e65-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": false
+}
